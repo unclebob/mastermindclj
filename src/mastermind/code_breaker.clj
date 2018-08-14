413dc2503e7a66b8521df82da60f82ee58eab110
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 09:52:53 -0500

    Starting on the decoder.  Got some future tests and the guess-to-number function written

--- src/mastermind/code_breaker.clj
+++ src/mastermind/code_breaker.clj
@@ -1,4 +1,10 @@
 (ns mastermind.code-breaker)
 
+(defn guess-to-number [guess]
+  (reduce #(+ (* 6 %1) %2) guess))
+
+(defn inc-guess [guess]
+  (let [guess-number (guess-to-number guess)]))
+
 (defn break-code [past-guesses]
   [0 0 0 0])
--- test/mastermind/code_breaker_test.clj
+++ test/mastermind/code_breaker_test.clj
@@ -1,11 +1,31 @@
 (ns mastermind.code-breaker-test
   (:require [midje.sweet :refer :all]
-            [mastermind.code-breaker :refer :all]))
+            [mastermind.code-breaker :refer :all]
+            [mastermind.code-maker :as cm]))
 
 (facts
   "Code Breaker"
   (fact
+    "guess-to-number"
+    (guess-to-number [0 0 0 0]) => 0
+    (guess-to-number [0 0 0 1]) => 1
+    (guess-to-number [0 0 1 0]) => 6
+    (guess-to-number [0 0 1 1]) => 7
+    (guess-to-number [0 1 1 1]) => 43
+    (guess-to-number [1 1 1 1]) => 259
+    (guess-to-number [5 5 5 5]) => (dec (* 6 6 6 6)))
+
+  (future-fact
+    "increment guess"
+    (inc-guess [0 0 0 0]) => [0 0 0 1])
+
+  (fact
     "initial guess"
     (break-code []) => [0 0 0 0])
+
+  (future-fact
+    "Walk through solution of code [1 2 3 4]"
+    (break-code [[[0 0 0 0]
+                  (cm/score [1 2 3 4] [0 0 0 0])]]) => [0 0 0 1])
 
   )
