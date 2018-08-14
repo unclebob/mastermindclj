5128a43e7278f7470ebfaa9c254bad6c7e7e688c
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 05:45:37 -0500

    first commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject mastermind "0.0.1-SNAPSHOT"
+  :description "Functional Mastermind for Clean Code E54"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.2"]]}
+             ;; You can add dependencies that apply to `lein midje` below.
+             ;; An example would be changing the logging destination for test runs.
+             :midje {}})
+             ;; Note that Midje itself is in the `dev` profile to support
+             ;; running autotest in the repl.
+
+  
