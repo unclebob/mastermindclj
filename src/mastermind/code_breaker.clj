7851c230810b6e5f657bd9006aaff27547ae998b
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 10:15:57 -0500

    even uglier refactoring using recursive loop.  Bah.

--- src/mastermind/code_breaker.clj
+++ src/mastermind/code_breaker.clj
@@ -4,7 +4,12 @@
   (reduce #(+ (* 6 %1) %2) guess))
 
 (defn number-to-guess [n]
-  [(rem (quot n 216) 6) (rem (quot n 36) 6) (rem (quot n 6) 6) (rem n 6)])
+  ;[(rem (quot n 216) 6) (rem (quot n 36) 6) (rem (quot n 6) 6) (rem n 6)])
+  (loop [guess (list)
+         num n]
+    (if (= 4 (count guess))
+      guess
+      (recur (cons (rem num 6) guess) (quot num 6)))))
 
 (defn inc-guess [guess]
   (let [guess-number (guess-to-number guess)]))
