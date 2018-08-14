2620e5e1b271915ab4507fce3a274c9deb3f7555
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 08:02:38 -0500

    Alternate implementation of counting position matches

--- src/mastermind/code_maker.clj
+++ src/mastermind/code_maker.clj
@@ -1,4 +1,7 @@
 (ns mastermind.code-maker)
 
 (defn score [code guess]
-  [(count (filter some? (map #(if (= %1 %2) :pos) code guess)))])
+  ;[(count (filter some? (map #(if (= %1 %2) :pos) code guess)))]
+  [(reduce + (map #(if (= (first %) (second %)) 1 0) (partition 2 (interleave code guess))))]
+
+  )
