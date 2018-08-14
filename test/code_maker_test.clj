4de59c14602c1adc998935d296ffe3a7ec145f94
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 08:10:17 -0500

    add tests to saturate the solution

--- test/code_maker_test.clj
+++ test/code_maker_test.clj
@@ -9,10 +9,19 @@
     (score [0 0 0 0] [1 1 1 1]) => [0])
 
   (fact
-    "score guess with one :pos match"
+    "score guess with one position match"
     (score [0 0 0 0] [0 1 1 1]) => [1])
 
   (fact
-    "guess with two :pos matches"
-    (score [0 0 0 0] [0 1 1 0]) => [2])
+    "guess with two position matches"
+    (score [0 0 0 0] [0 1 1 0]) => [2]
+    (score [0 0 0 0] [1 0 1 0]) => [2]
+    (score [0 0 0 0] [0 1 0 1]) => [2]
+    )
+
+  (fact
+    "guess with many position matches"
+    (score [1 1 1 1] [0 1 1 1]) => [3]
+    (score [0 0 0 0] [0 0 0 1]) => [3]
+    (score [1 2 3 4] [1 2 3 4]) => [4])
   )
