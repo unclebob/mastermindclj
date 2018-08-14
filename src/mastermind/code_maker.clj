019c96916696d6e7e2a3041ae6408f718fffe442
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 09:18:36 -0500

    A nasty problem.  Confound 1.  If there are duplicate colours in the guess, they cannot all be awarded a key peg unless they correspond to the same number of duplicate colours in the hidden code.

--- src/mastermind/code_maker.clj
+++ src/mastermind/code_maker.clj
@@ -11,8 +11,20 @@
   (count-true
     (map #(contains? (set code) %1) guess)))
 
+(defn count-of [value values]
+  (count (filter #(= value %) values)))
+
+(defn over-count [code guess]
+  (let [code-values (set code)]
+    (reduce +
+            (filter pos?
+                    (map #(- (count-of % guess) (count-of % code))
+                         code-values)))
+    ))
+
 (defn score [code guess]
   (let [p (position-matches code guess)
-        v (value-matches code guess)]
-    [p (- v p)])
+        v (value-matches code guess)
+        o (over-count code guess)]
+    [p (- v p o)])
   )
--- test/code_maker_test.clj
+++ test/code_maker_test.clj
@@ -51,4 +51,6 @@
   they cannot all be awarded a key peg unless they correspond
   to the same number of duplicate colours in the hidden code."
   (score [1 2 3 4] [3 3 3 4]) => [2 0]
+  (score [1 1 2 3] [1 2 1 2]) => [1 2]
+  (score [3 3 3 3] [3 1 1 1]) => [1 0]
   )
