bbd32ece592ca5c2002db5b8c4b4c61145937c9f
Robert C. Martin <unclebob@cleancoder.com>
2018-08-14 08:06:00 -0500

    Revert and refactor original implementation

--- src/mastermind/code_maker.clj
+++ src/mastermind/code_maker.clj
@@ -1,7 +1,5 @@
 (ns mastermind.code-maker)
 
 (defn score [code guess]
-  ;[(count (filter some? (map #(if (= %1 %2) :pos) code guess)))]
-  [(reduce + (map #(if (= (first %) (second %)) 1 0) (partition 2 (interleave code guess))))]
-
+  [(count (filter identity (map #(= %1 %2) code guess)))]
   )
